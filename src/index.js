d4ab8bee61c38c5884267cbcb048dd52cc03e17e
yuladevin <yuladevin@gmail.com>
2019-04-05 11:20:38 +0300

    first push

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './js/common'
+
+import './css/main.css'
+//import './scss/main.scss'
+
+//import 'vue'
+//import 'bootstrap/dist/css/bootsrap.min.css'
+//import Vue from 'vue'
+
+window.Vue = require('vue')
